96676c0fe81cf7bdc7f549dd35a2b9b21664d0f2
José Cleyton <cleytoncomp@gmail.com>
2021-04-14 16:30:42 -0300

    CM-8 - Alteração do comportamento do botão de filtro

--- church-manager/src/app/page-header/page-header.module.ts
+++ church-manager/src/app/page-header/page-header.module.ts
@@ -5,9 +5,21 @@ import { CommonModule } from '@angular/common';
 import { MatDialogModule } from '@angular/material/dialog';
 import { DialogModule } from './dialog/dialog.module';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 @NgModule({
   declarations: [PageHeaderComponent],
-  imports: [CommonModule, MatDialogModule, DialogModule, MatButtonModule, MatIconModule],
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    DialogModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+  ],
   exports: [PageHeaderComponent],
 })
 export class PageHeaderModule {}
